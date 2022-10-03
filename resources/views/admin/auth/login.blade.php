dd26910c783b3f4a2d178017f00f1e7c70ddfd21
mdshajibazher <mdshajibazher@gmail.com>
2022-10-03 07:04:21 +0600

    another test update

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -57,7 +57,7 @@
                                 @enderror
                             </div>
                             <div class="form-group">
-                                <button id="login-button" type="submit" class="form-control btn btn-primary rounded submit px-3">SignIn (updated again)</button>
+                                <button id="login-button" type="submit" class="form-control btn btn-primary rounded submit px-3">Another Update</button>
                             </div>
                             <div class="form-group d-md-flex">
                                 <div class="w-50 text-left">
