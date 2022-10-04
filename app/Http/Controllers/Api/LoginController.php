79f681f94ce3595badbc4c660e72aeefc3af2415
mdshajibazher <mdshajibazher@gmail.com>
2022-10-04 09:07:14 +0600

    Remove unused

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
