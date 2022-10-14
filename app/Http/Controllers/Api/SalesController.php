924d13186f02634302caea739fe5a60474c0f2e8
mdshajibazher <mdshajibazher@gmail.com>
2022-10-14 11:04:56 +0600

    paginate_per_page dynamic

--- app/Http/Controllers/Api/SalesController.php
+++ app/Http/Controllers/Api/SalesController.php
@@ -8,8 +8,11 @@ use Illuminate\Http\Request;
 
 class SalesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-       return Sale::with('user')->withTrashed()->orderBy('sales_at','DESC')->paginate(10);
+        $per_page = $request->per_page ?? 10;
+        $orderBy = $request->order_by ?? 'sales_at';
+        $orderByDir = $request->order_by_dir ?? 'DESC';
+        return Sale::with('user')->withTrashed()->orderBy($orderBy,$orderByDir)->paginate($per_page);
     }
 }
